137f55d249a3d7996d850a3b966eeec74cc873a8
katapad <katapad@gmail.com>
2015-04-28 17:29:03 +0900

    Automated commit at 2015-04-28 08:29:03 UTC by middleman-deploy 1.0.0

--- js/requestAnimationFrame.js
+++ js/requestAnimationFrame.js
@@ -1,1 +1,1 @@
-(function(){window.requestAnimationFrame||(window.requestAnimationFrame=function(){return window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(n){window.setTimeout(n,1e3/60)}}())}).call(this);
+(function(){window.requestAnimationFrame||(window.requestAnimationFrame=window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame)}).call(this);
